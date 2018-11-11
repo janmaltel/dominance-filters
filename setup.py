ae4f326a63bb3ac895ad63074310d64865cc1fab
bogaaak <j.m.lichtenberg@bath.ac.uk>
2018-11-11 17:44:10 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='domtools',
+      version='0.1',
+      description='Dominance filter utils',
+      # url='http://github.com/storborg/funniest',
+      author='Jan Malte Lichtenberg',
+      author_email='jml82@bath.ac.uk',
+      license='MIT',
+      packages=['domtools'],
+      zip_safe=False)
